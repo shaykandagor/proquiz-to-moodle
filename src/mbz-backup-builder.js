aa5c33a88905737a5191668161d7304d248ff1df
shaykandagor <you@example.com>
2024-11-06 16:31:41 +0200

    Included file xml script: wip(closing tags)

--- src/mbz-backup-builder.js
+++ src/mbz-backup-builder.js
@@ -90,6 +90,33 @@ function createMoodleBackup(outputDir) {
         .end({ pretty: true });
 
     fs.writeFileSync(path.join(outputDir, 'moodle_backup.xml'), backupXml);
+
+    const fileXml = xmlbuilder.create('files', { encoding: 'UTF-8' })
+        .ele('file', '').up()
+            .ele('contenthash', '').up()
+            .ele('contextid', '').up()
+            .ele('component').up()
+            .ele('filearea').up()
+            .ele('itemid').up()
+            .ele('filepath').up()
+            .ele('filename').up()
+            .ele('userid').up()
+            .ele('filesize').up()
+            .ele('mimetype').up()
+            .ele('status').up()
+            .ele('timecreated').up()
+            .ele('timemodified').up()
+            .ele('source').up()
+            .ele('author').up()
+            .ele('license').up()
+            .ele('sortorder').up()
+            .ele('repositorytype').up()
+            .ele('repositoryid').up()
+            .ele('reference').up()
+        .end({ pretty: true });
+
+    fs.writeFileSync(path.join(outputDir, 'files.xml'), fileXml);
+
 }
 
 // Example usage:
