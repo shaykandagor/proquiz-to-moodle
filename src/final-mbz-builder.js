81055d1d82a01ae8098b4aca18d92337c82e7f7a
ainaral <larranagaainara@gmail.com>
2024-11-26 16:45:03 +0200

    Refactor final-mbz-builder.js to create output directory if it doesn't exist

--- src/final-mbz-builder.js
+++ src/final-mbz-builder.js
@@ -1,14 +1,23 @@
+fs = require('fs');
 const path = require('path');
 const { buildGroupsXml } = require('./json-to-mbz/groups');
 const { buildCoursesXml } = require('./json-to-mbz/course/course');
 
-const finalDir = path.join(__dirname, '..', 'final-mbz');
+const finalDir = path.join('final-mbz');
+const outputDir = path.join(__dirname, '..', 'output', finalDir);
+
+// Ensure the output directory exists
+if (!fs.existsSync(outputDir)) {
+fs.mkdirSync(outputDir, { recursive: true });
+}
+
 const groupsJsonFilePath = "./exported_data/json/export-file-groups-2024-07-01-11-30-19.json";
 const courseJsonFilePath = "./exported_data/json/export-file-sfwd-courses-2024-07-01-11-30-19.json";
 
 function createFinalMoodleBackup(finalDir, groupsJsonFilePath, courseJsonFilePath) {
+
     buildGroupsXml(groupsJsonFilePath, finalDir);
-    buildCoursesXml(courseJsonFilePath, finalDir);
+    //buildCoursesXml(courseJsonFilePath, finalDir);
 }
 
 
