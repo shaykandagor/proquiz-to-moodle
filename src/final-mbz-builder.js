692d5b351fdd59fd154966c94205b2a6e0ecd2e4
shaykandagor <you@example.com>
2024-11-26 12:24:22 +0200

    solved directory path error

--- src/final-mbz-builder.js
+++ src/final-mbz-builder.js
@@ -1,15 +1,22 @@
+const fs = require('fs');
 const path = require('path');
 const { buildGroupsXml } = require('./json-to-mbz/groups');
 const { buildCoursesXml } = require('./json-to-mbz/course/course');
 
-const finalDir = path.join(__dirname, '..', 'final-mbz');
+const finalDir = path.join('final-mbz');
+const outputDir = path.join(__dirname, '..', 'output', finalDir);
+
+// Ensure the output directory exists
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+}
+
 const groupsJsonFilePath = "./exported_data/json/export-file-groups-2024-07-01-11-30-19.json";
 const courseJsonFilePath = "./exported_data/json/export-file-sfwd-courses-2024-07-01-11-30-19.json";
 
-function createFinalMoodleBackup(finalDir, groupsJsonFilePath, courseJsonFilePath) {
-    buildGroupsXml(groupsJsonFilePath, finalDir);
-    buildCoursesXml(courseJsonFilePath, finalDir);
+function createFinalMoodleBackup(outputDir, groupsJsonFilePath, courseJsonFilePath) {
+    buildGroupsXml(groupsJsonFilePath, outputDir);
+    // buildCoursesXml(courseJsonFilePath, outputDir);
 }
 
-
-createFinalMoodleBackup(finalDir, groupsJsonFilePath, courseJsonFilePath);
+createFinalMoodleBackup(outputDir, groupsJsonFilePath, courseJsonFilePath);
