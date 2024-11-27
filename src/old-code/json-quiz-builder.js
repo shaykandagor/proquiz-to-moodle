92b25ebedad63d2bacf9b60f45e2f9861bfc18b7
Juha Tauriainen <juha@bin.fi>
2024-11-27 20:58:21 +0200

    Store raw json log

--- src/old-code/json-quiz-builder.js
+++ src/old-code/json-quiz-builder.js
@@ -1,5 +1,27 @@
 const fs = require("fs");
-const { parse } = require("../utils/quiz-parser");
+const {
+  parse,
+  unserialize,
+  parseAnswer,
+  removeEscapeChars,
+} = require("../utils/quiz-parser");
+
+const makeRawLog = (data, outputPath) => {
+  const parsedData = JSON.parse(data);
+  parsedData.quiz_pro.questions = parsedData.quiz_pro.questions
+    .map(unserialize)
+    .map((question) => ({
+      ...question,
+      answer_data: unserialize(removeEscapeChars(question.answer_data)).map(
+        parseAnswer
+      ),
+    }));
+
+  fs.writeFileSync(
+    `${outputPath}.raw.json`,
+    JSON.stringify(parsedData, null, 2)
+  );
+};
 
 const readFileCallback = (outputPath) => (err, data) => {
   if (err) {
@@ -10,6 +32,8 @@ const readFileCallback = (outputPath) => (err, data) => {
   const content = parse(data);
 
   fs.writeFileSync(outputPath, JSON.stringify(content, null, 2));
+
+  makeRawLog(data, outputPath);
 };
 
 const buildQuizJson = (path, outputPath) =>
