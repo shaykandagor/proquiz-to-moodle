afdd1a9243d64950fe6a215d1041afa9635acca9
ainaral <larranagaainara@gmail.com>
2024-11-29 09:50:56 +0200

    Fill in book xml works including the chapters

--- src/json-to-mbz/activities/book.js
+++ src/json-to-mbz/activities/book.js
@@ -2,9 +2,12 @@ const fs = require("fs");
 const path = require("path");
 const xml2js = require("xml2js");
 const he = require("he");
+const { time } = require("console");
+const { title } = require("process");
 
 const generateRandomNumber = () => parseInt(Math.random() * 10000000);
-const att_id = () => parseInt(Math.random() * 20000);
+const bookatt_id = () => parseInt(Math.random() * 10000);
+const chapter_id = () => parseInt(Math.random() * 20000);
 
 // Function to create content from JSON data
 const createContent = (data) => {
@@ -15,33 +18,43 @@ const createContent = (data) => {
       wp_post_title,
       wp_post_content,
       wp_post_date_gmt,
+      wp_post_date,
     } = post;
 
     const context_id = generateRandomNumber();
-    const page_id = att_id();
+    const book_id = bookatt_id();
+    const chapterid = chapter_id();
 
     return {
+      id: book_id,
+      contextid: context_id,
       moduleid: wp_post_id,
       name: wp_post_title,
-      content: wp_post_content,
+      intro: wp_post_content,
       timemodified: wp_post_date_gmt,
+      timecreated: wp_post_date,
+      introformat: 1,
+      numbering: 1,
+      navstyle: 1,
+      customtitles: 0,
+      // inside chapters
+      chapterid: chapterid,
+      pagenum: 1,
+      subchapter: 0,
+      title: "Chapter 1" || "",
+      content: "Chapter 1 content" || "",
       contentformat: 1,
-      legacyfiles: 0,
-      legacyfileslast: "$@NULL@$",
-      display: 5,
-      displayoptions: {
-        printintro: 0,
-        printlastmodified: 1,
-      },
-      revision: 1,
-      id: page_id,
-      contextid: context_id
+      hidden: 0,
+      timemodified: "",
+      importsrc: "",
+      //chapters: jsonData.topics
+      chapters: [],
     };
   });
 };
 
 // Function to update XML with JSON content
-const updateXmlWithJsonContent = (xmlData, jsonContent) => {
+const updateBookXmlWithJsonContent = (xmlData, jsonContent) => {
   const parser = new xml2js.Parser();
   const builder = new xml2js.Builder({
     xmldec: { standalone: null, encoding: "UTF-8" },
@@ -53,29 +66,38 @@ const updateXmlWithJsonContent = (xmlData, jsonContent) => {
       if (err) {
         return reject(err);
       }
-      // TODO: Fix XML content 
-      // Encode the HTML content for XML
-      const encodedContent = he.encode(jsonContent.content, {
-        useNamedReferences: true,  // Use named entities like &lt; for readability
-        allowUnsafeSymbols: true  // Allow characters like '>' and '"'
-      });
+      
+      // Clear existing book in XML
+     // result.chapters = { book: [] };
 
       // Update the XML content with JSON content
       result.activity.$.id = jsonContent.id;
       result.activity.$.moduleid = jsonContent.moduleid;
       result.activity.$.contextid = jsonContent.contextid;
-      result.activity.page[0].$.id= jsonContent.id;
-      result.activity.page[0].name[0] = jsonContent.name;
-      result.activity.page[0].intro[0] = "";
-      result.activity.page[0].introformat[0] = jsonContent.contentformat;
-      result.activity.page[0].content[0] = jsonContent.content;
-      result.activity.page[0].contentformat[0] = jsonContent.contentformat;
-      result.activity.page[0].legacyfiles[0] = jsonContent.legacyfiles;
-      result.activity.page[0].legacyfileslast[0] = jsonContent.legacyfileslast;
-      result.activity.page[0].display[0] = jsonContent.display;
-      result.activity.page[0].displayoptions[0] = JSON.stringify(jsonContent.displayoptions);
-      result.activity.page[0].revision[0] = jsonContent.revision;
-      result.activity.page[0].timemodified[0] = jsonContent.timemodified;
+      result.activity.book[0].$.id= jsonContent.id;
+      result.activity.book[0].name[0] = jsonContent.name;
+      result.activity.book[0].intro[0] = jsonContent.intro;
+      result.activity.book[0].introformat[0] = jsonContent.contentformat;
+      result.activity.book[0].numbering[0] = jsonContent.numbering;
+      result.activity.book[0].navstyle[0] = jsonContent.navstyle;
+      result.activity.book[0].customtitles[0] = jsonContent.customtitles;
+      result.activity.book[0].timecreated[0] = jsonContent.timecreated;
+      result.activity.book[0].timemodified[0] = jsonContent.timemodified;
+
+      // Update chapters 
+      // TODO: data is found in topics json
+      /* result.activity.book[0].chapters[0].chapter.forEach((chapter, index) => {
+        const jsonChapter = jsonContent.chapters[index];
+        chapter.$.id = jsonChapter.id;
+        chapter.pagenum[0] = jsonChapter.pagenum;
+        chapter.subchapter[0] = jsonChapter.subchapter;
+        chapter.title[0] = jsonChapter.title;
+        chapter.content[0] = he.encode(jsonChapter.content, { useNamedReferences: true });
+        chapter.contentformat[0] = jsonChapter.contentformat;
+        chapter.hidden[0] = jsonChapter.hidden;
+        chapter.timemodified[0] = jsonChapter.timemodified;
+        chapter.importsrc[0] = jsonChapter.importsrc;
+      }); */
 
       const updatedXml = builder.buildObject(result);
       resolve(updatedXml);
@@ -84,7 +106,7 @@ const updateXmlWithJsonContent = (xmlData, jsonContent) => {
 };
 
 // Function to read, update, and write XML files
-const processPageXmlFiles = (jsonFilePath, xmlDirPath) => {
+const processBookXmlFiles = (jsonFilePath, xmlDirPath) => {
 
   fs.readFile(jsonFilePath, "utf8", (err, jsonData) => {
     if (err) {
@@ -95,11 +117,11 @@ const processPageXmlFiles = (jsonFilePath, xmlDirPath) => {
     const jsonContentArray = createContent(jsonData);
 
      // List directories in xmlDirPath that start with "page_"
-     const pageDirs = fs.readdirSync(xmlDirPath).filter(dir => dir.startsWith('page_'));
+     const pageDirs = fs.readdirSync(xmlDirPath).filter(dir => dir.startsWith('book_'));
 
      pageDirs.forEach((dir) => {
        const dirPath = path.join(xmlDirPath, dir);
-       const xmlFilePath = path.join(dirPath, "page.xml");
+       const xmlFilePath = path.join(dirPath, "book.xml");
  
        // Extract the folder ID from the directory name
        const folderId = dir.split('_')[1];
@@ -112,7 +134,7 @@ const processPageXmlFiles = (jsonFilePath, xmlDirPath) => {
          return;
        }
  
-       console.log(`Processing page XML file: ${xmlFilePath}`);
+       console.log(`Processing book XML file: ${xmlFilePath}`);
  
        fs.readFile(xmlFilePath, "utf8", (err, xmlData) => {
          if (err) {
@@ -120,7 +142,7 @@ const processPageXmlFiles = (jsonFilePath, xmlDirPath) => {
            return;
          }
  
-         updateXmlWithJsonContent(xmlData, jsonContent, folderId)
+         updateBookXmlWithJsonContent(xmlData, jsonContent, folderId)
            .then((updatedXml) => {
              fs.writeFile(xmlFilePath, updatedXml, "utf8", (err) => {
                if (err) {
@@ -138,4 +160,4 @@ const processPageXmlFiles = (jsonFilePath, xmlDirPath) => {
   });
 };
 
-module.exports = { processPageXmlFiles };
+module.exports = { processBookXmlFiles };
--- src/json-to-mbz/activities/lessons.js
+++ src/json-to-mbz/activities/lessons.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const createActivitiesFolders = require("./create-activities-folders");
 const { processPageXmlFiles } = require("./page");
+const { processBookXmlFiles } = require("./book");
 
 function buildLessonsXml(lessonsJsonFilePath, finalDir) {
   // Read the JSON file to get the ids and pass its content to createActivitiesFolders
@@ -15,7 +16,8 @@ function buildLessonsXml(lessonsJsonFilePath, finalDir) {
     console.log("Activities folders created successfully at ", finalDir);
 
     // Process XML files
-    processPageXmlFiles(lessonsJsonFilePath, path.join(finalDir, "activities"));
+    //processPageXmlFiles(lessonsJsonFilePath, path.join(finalDir, "activities"));
+    processBookXmlFiles(lessonsJsonFilePath, path.join(finalDir, "activities"));
     console.log("XMLs created ", finalDir);
   });
 }
