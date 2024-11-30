1577ac6bc1577241a9e81d4cbb5f5fdb5e75d7ea
ainaral <larranagaainara@gmail.com>
2024-11-30 11:58:42 +0200

    summary is populated with json data. TODO: separate summary content to separate sections folders

--- src/json-to-mbz/sections/section.js
+++ src/json-to-mbz/sections/section.js
@@ -5,70 +5,48 @@ const xml2js = require("xml2js");
 const createContent = (data) => {
     const jsonData = JSON.parse(data);
     return jsonData.wp_data.map((post) => {
-        const {
-            wp_post_content
-        } = post;
+
+        const { wp_post_id, wp_post_content } = post;
 
         return {
+            section_id_attribute: wp_post_id,
             summary: wp_post_content
         };
     });
 }
 
-// Function to update XML with JSON content
-const updateXmlWithJsonContent = (xmlData, jsonContent, folderId) => {
-    const parser = new xml2js.Parser();
-    const builder = new xml2js.Builder({
-      xmldec: { standalone: null, encoding: "UTF-8" },
-      cdata: true
-    });
-  
-    return new Promise((resolve, reject) => {
-      parser.parseString(xmlData, (err, result) => {
-        if (err) {
-          return reject(err);
-        }
-
-        result.section.summary[0] = [jsonContent.summary]; 
-  
-        const updatedXml = builder.buildObject(result);
-        resolve(updatedXml);
-      });
-    });
-};
-
 // Function to update module.xml with static content
-const updateSectionXmlWithStaticContent = (xmlData, folderId) => {
-    const parser = new xml2js.Parser();
-    const builder = new xml2js.Builder({
-      xmldec: { standalone: null, encoding: "UTF-8" },
-      cdata: true
+const updateSectionXml = (xmlData, jsonContent, folderId) => {
+  const parser = new xml2js.Parser();
+  const builder = new xml2js.Builder({
+    xmldec: { standalone: null, encoding: "UTF-8" },
+    cdata: true
+  });
+
+  const timestamp = Math.floor(Date.now() / 1000);
+
+  return new Promise((resolve, reject) => {
+    parser.parseString(xmlData, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      // Update the XML content with static values
+      result.section.$.id = folderId;
+      result.section.number = ["0"]; 
+      result.section.name = ["$@NULL@$"]; 
+      result.section.summary = jsonContent[0].summary;
+      result.section.summaryformat = ["1"]; 
+      result.section.sequence = [""]; 
+      result.section.visible = ["1"]; 
+      result.section.availabilityjson = ["$@NULL@$"]; 
+      result.section.timemodified = [timestamp]; 
+
+      const updatedXml = builder.buildObject(result);
+      resolve(updatedXml);
     });
-  
-    const timestamp = Math.floor(Date.now() / 1000);
-  
-    return new Promise((resolve, reject) => {
-      parser.parseString(xmlData, (err, result) => {
-        if (err) {
-          return reject(err);
-        }
-  
-        // Update the XML content with static values
-        result.section.$.id = folderId;
-        result.section.number = ["0"]; 
-        result.section.name = ["$@NULL@$"]; 
-        result.section.summary = [""]; 
-        result.section.summaryformat = ["1"]; 
-        result.section.sequence = [""]; 
-        result.section.visible = ["1"]; 
-        result.section.availabilityjson = ["$@NULL@$"]; 
-        result.section.timemodified = [timestamp]; 
-  
-        const updatedXml = builder.buildObject(result);
-        resolve(updatedXml);
-      });
-    });
-  };
+  });
+};
 
 // Function to read, update, and write XML files
 const processSectionXmlFiles = (jsonFilePath, outputDir) => {
@@ -78,7 +56,7 @@ const processSectionXmlFiles = (jsonFilePath, outputDir) => {
             return;
         }
 
-        const content = createContent(jsonData);
+        const contentArray = createContent(jsonData);
 
         // Read all directories in the base directory
         fs.readdir(outputDir, (err, directories) => {
@@ -92,27 +70,18 @@ const processSectionXmlFiles = (jsonFilePath, outputDir) => {
                 dir.startsWith("section_")
             );
 
-            // Process each section directory
-            sectionDirs.forEach((sectionDir) => {
-                const sectionPath = path.join(outputDir, sectionDir);
-                const sectionXmlFilePath = path.join(sectionPath, "section.xml"); // Assuming XML files are named "section.xml"
-                const folderId = sectionDir.split("_")[1]; // Extract the folder ID from "section_id"
+          // Process each section directory
+          sectionDirs.forEach((sectionDir) => {
+              const sectionPath = path.join(outputDir, sectionDir);
+              const sectionXmlFilePath = path.join(sectionPath, "section.xml"); 
+              const folderId = sectionDir.split("_")[1]; // Extract the folder ID from "section_id"
 
-                // Check if the XML file exists
-                if (fs.existsSync(sectionXmlFilePath)) {
-                const xmlData = fs.readFileSync(sectionXmlFilePath, "utf8");
+              // Check if the XML file exists
+              if (fs.existsSync(sectionXmlFilePath)) {
+              const xmlData = fs.readFileSync(sectionXmlFilePath, "utf8");
 
-                // // Update the XML with JSON content
-                // updateXmlWithJsonContent(xmlData, content[index], folderId) // Use the corresponding JSON content
-                //     .then((updatedXml) => {
-                //         fs.writeFileSync(xmlFilePath, updatedXml, "utf8");
-                //         console.log("Updated section XML file successfully:", xmlFilePath);
-                //     })
-                //     .catch((err) => {
-                //         console.error("Error updating section XML file:", err);
-                //     });
-                // Update the section XML with static content
-                updateSectionXmlWithStaticContent(xmlData, folderId)
+              // Update the section XML files
+              updateSectionXml(xmlData, contentArray, folderId)
                 .then((updatedXml) => {
                     fs.writeFileSync(sectionXmlFilePath, updatedXml, "utf8");
                     console.log("Updated section XML file successfully:", sectionXmlFilePath);
@@ -120,9 +89,10 @@ const processSectionXmlFiles = (jsonFilePath, outputDir) => {
                 .catch((err) => {
                     console.error("Error updating section XML file:", err);
                 });
-                } else {
-                    console.warn("section XML file not found in:", sectionPath);
-                }
+
+              } else {
+                  console.warn("section XML file not found in:", sectionPath);
+              }
             });
         });
     });
