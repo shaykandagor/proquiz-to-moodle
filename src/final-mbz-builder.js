4bbf153e10f7810b387af635de14e12d2fd5fd6e
ainaral <larranagaainara@gmail.com>
2024-12-04 15:55:34 +0200

    main calls course and lesson builders

--- src/final-mbz-builder.js
+++ src/final-mbz-builder.js
@@ -6,6 +6,8 @@ const processSectionXmlFiles = require("./json-to-mbz/sections/section");
 const buildCourseXml = require("./json-to-mbz/course/build-course");
 const generateMainFiles = require("./json-to-mbz/main-files/create-main-files");
 const jsonContent = require("./json-to-mbz/sections/jsonContent");
+const sectionsJsonContent = require("./json-to-mbz/sections/jsonContent");
+const buildSections = require("./json-to-mbz/sections/build-sections");
 
 // const finalDir = path.join(__dirname, "..", "final-mbz");
 const finalDir = path.join(__dirname, "..", "test-final-mbz");
@@ -29,16 +31,15 @@ const topicsJsonFilePath = "./exported_data/json/export-file-sfwd-topic-2024-07-
 async function createFinalMoodleBackup() {
 
   // Step 1: Create course folder
-  //await buildCourseXml(courseJsonFilePath, finalDir); // creates course folder
+  await buildCourseXml(courseJsonFilePath, finalDir); // creates course folder
   // Step 2: Create activities folder
-  //await buildLessonsXml(lessonsJsonFilePath, topicsJsonFilePath, finalDir); // creates activities folder
+  await buildLessonsXml(lessonsJsonFilePath, topicsJsonFilePath, finalDir); // creates activities folder
   // Step 3: Create sections folder
+  // await buildSections(finalDir)
+  //createSectionsFolders(finalDir); // creates sections folder
 
-  /* const sectionsDir = path.join(finalDir, "sections");
-  const startId = 5630;
-  const numberOfSections = 4; */
-  createSectionsFolders(finalDir); // creates sections folder
-  // jsonContent();
+  //jsonContent();
+  //sectionsJsonContent(finalDir);
   // processSectionXmlFiles(sectionsJsonFilePath, sectionsDir);
 
   // Step 4: Create main files
