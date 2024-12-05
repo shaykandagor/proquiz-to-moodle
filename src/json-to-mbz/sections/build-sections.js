c8e8c08df8cfbaaf909eeaac8b27bc1aedf0a395
ainaral <larranagaainara@gmail.com>
2024-12-05 10:44:27 +0200

    update sequence tag

--- src/json-to-mbz/sections/build-sections.js
+++ src/json-to-mbz/sections/build-sections.js
@@ -1,8 +1,8 @@
+const path = require("path");
 const createSectionsFolders = require("./create-section-folders");
-const jsonContent = require("./jsonContent");
 const generateCombinedJsonData = require("./jsonContent");
-const sectionsJsonContent = require("./jsonContent");
 const processSectionXmlFiles = require("./section");
+const updateSectionXmlWithActivities = require("./section-sequence");
 
 async function buildSections(finalDir) {
   // step 1: create section folder and xml files
@@ -14,6 +14,13 @@ async function buildSections(finalDir) {
   // step 3: update section xml files using JSON content
   const sectionsJsonFilePath = "./exported_data/json/combined_sections.json";
   await processSectionXmlFiles(sectionsJsonFilePath, finalDir);
+
+  // step 4: update section.xml sequence tag with activity IDs
+  const activitiesDir = path.join(finalDir, "activities");
+  const sectionsDir = path.join(finalDir, "sections");
+  const targetSection = "section_5634"; // The section to modify
+
+  await updateSectionXmlWithActivities(activitiesDir, sectionsDir, targetSection);
 
 }
 
